9c412db558a1ac6758aea4613be0846a052b2489
Lorrain Tapioca <brearey88@yandex.ru>
2025-07-11 19:57:36 +0900

    express started

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello')
+import express from 'express'
+const expressApp = express()
+
+expressApp.get('/ping', (req, res) => {
+  return res.send({ message: 'pong' })
+})
+
+try {
+  const port = 5000
+  expressApp.listen(port, 'localhost', () => {
+    console.log(`server started on port: ${port}`)
+  })
+} catch (error) {
+  console.error(error)
+  process.exit(1)
+}
