f2354334bc50bae4d852df52fa449a0cfffaf7f1
Lorrain Tapioca <brearey88@yandex.ru>
2025-07-11 12:25:07 +0900

    simple ideas list on h2

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,6 +1,21 @@
-import React from "react";
-
 export const App = () => {
-    // return <h1>Hello world</h1>
-    return React.createElement("h1", {}, "Hello, world")
+    return (
+        <div>
+            <h1>Ideanick</h1>
+            <div>
+                <div>
+                    <h2>Idea 1</h2>
+                    <p>Idea 1 description</p>
+                </div>
+                <div>
+                    <h2>Idea 2</h2>
+                    <p>Idea 2 description</p>
+                </div>
+                <div>
+                    <h2>Idea 3</h2>
+                    <p>Idea 3 description</p>
+                </div>
+            </div>
+        </div>
+    );
 }
