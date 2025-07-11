332daa09a2cc8855a92986d0e20087293af63e64
Lorriant <brearey4@gmail.com>
2025-07-11 21:42:25 +0900

    lesson 14 done (trpc created)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,14 @@
 import express from 'express'
-const expressApp = express()
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRouter } from './trpc'
 
-const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Idea 1 description' },
-  { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Idea 2 description' },
-  { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Idea 3 description' },
-  { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Idea 4 description' },
-  { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Idea 5 description' },
-]
+const expressApp = express()
 
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
 })
 
-expressApp.get('/ideas', (req, res) => {
-  res.send(ideas)
-})
+expressApp.use('/trpc', trpcExpress.createExpressMiddleware({ router: trpcRouter }))
 
 expressApp.listen(3000, () => {
   console.log(`server started on http://localhost:3000`)
