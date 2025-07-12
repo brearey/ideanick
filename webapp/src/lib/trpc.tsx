74a28b6461abf5c492c5cf1531a62f4f452490d8
Lorriant <brearey4@gmail.com>
2025-07-12 10:44:00 +0900

    fixed bug "@ideanick/backend": "workspace:../backend",

--- webapp/src/lib/trpc.tsx
+++ webapp/src/lib/trpc.tsx
@@ -1,4 +1,4 @@
-import type { TrpcRouter } from '@ideanick/backend/src/trpc'
+import type { TrpcRouter } from '@ideanick/backend/src/trpc.ts'
 import { createTRPCReact } from '@trpc/react-query'
 
 const trpc = createTRPCReact<TrpcRouter>()
